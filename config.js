29e62b021df300c3003246e2dae84df897de6b7e
Rahul24-gif <rahul.mo2410@gmail.com>
2025-12-26 00:06:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDuV3QgG8K5DDooao4Hjd2Ixi11sCjOhPk",
+  authDomain: "study-15285.firebaseapp.com",
+  projectId: "study-15285",
+  storageBucket: "study-15285.firebasestorage.app",
+  messagingSenderId: "44232947532",
+  appId: "1:44232947532:web:785c2e11e33901233ac1e9",
+  measurementId: "G-PPPEGWMJ01"
+};
